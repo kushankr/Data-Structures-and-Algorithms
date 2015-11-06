b4ff8005dd1bdfd290d71c091c55ac509c600f36
kushankr <kushankr@umich.edu>
2015-11-06 12:45:18 -0500

    Code Sample

---
+++ BinarySearch_code.py
@@ -0,0 +1,22 @@
+#binary search
+
+
+def binary_search(mylist,item):
+	midpoint = len(mylist)//2
+	if len(mylist) == 0:
+		return False;
+
+	if mylist[midpoint] == item:
+		return True;
+
+	else:
+		if item < mylist[midpoint]:
+			return binary_search(mylist[:midpoint],item)
+
+		else:
+			return binary_search(mylist[midpoint+1:],item)
+
+
+#checking
+print binary_search([0,1,2,8,13,17,19,32,42],41);
+print binary_search([0,1,2,8,13,17,19,32,42],42);
